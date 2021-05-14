f152bf9120df292098409a0b2cb99df03f3b045e
booldook <booldook@gmail.com>
2021-05-14 15:59:32 +0900

    글 작성 프로토타입 완료

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 /*************** 글로벌 설정 *****************/
 var auth = firebase.auth();	//firebase의 auth(인증)모듈을 불러온다.
 var googleAuth = new firebase.auth.GoogleAuthProvider(); //구글로그인 모듈을 불러온다.
+var db = firebase.database(); //firebase의 database모듈을 불러온다.
 var user = null;
 
 
@@ -16,6 +17,35 @@ $('.bt-logout').click(onLogOut);
 
 
 /*************** 이벤트 콜백 *****************/
+function onSubmit(f) {
+	if(f.writer.value.trim() === '') {
+		alert('작성자는 필수사항 입니다.');
+		f.writer.focus();
+		return false;
+	}
+	
+	if(f.content.value.trim() === '') {
+		alert('한 줄 내용은 필수사항 입니다.');
+		f.content.focus();
+		return false;
+	}
+
+	var data = {
+		writer: f.writer.value,
+		content: f.content.value,
+		createdAt: new Date().getTime(),
+		uid: user.uid,
+		readnum: 0
+	}
+	if(user && user.uid) db.ref('root/board/').push(data);
+	else alert('정상적인 접근이 아닙니다.');
+	
+	f.content.value = '';
+	f.content.focus();
+
+	return false;
+}
+
 function onChangeAuth(r) {
 	user = r;
 	console.log(user);
@@ -23,6 +53,8 @@ function onChangeAuth(r) {
 		$('.header-wrapper .email').text(user.email);
 		$('.header-wrapper .photo img').attr('src', user.photoURL);
 		$('.header-wrapper .info-wrap').css('display', 'flex');
+		$('.create-wrapper').show();
+		$('.create-wrapper input[name="writer"]').val(user.displayName);
 		$('.bt-login').hide();
 		$('.bt-logout').show();
 	}
@@ -30,6 +62,8 @@ function onChangeAuth(r) {
 		$('.header-wrapper .email').text('');
 		$('.header-wrapper .photo img').attr('src', '//via.placeholder.com/1x1/333');
 		$('.header-wrapper .info-wrap').css('display', 'none');
+		$('.create-wrapper').hide();
+		$('.create-wrapper input[name="writer"]').val('');
 		$('.bt-login').show();
 		$('.bt-logout').hide();
 	}
