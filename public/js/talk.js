68fcce2e8c24099136c2dc8332e8bc3d7d216bdc
booldook <booldook@gmail.com>
2021-05-21 12:03:02 +0900

    화면 세개로 분할

--- public/js/talk.js
+++ public/js/talk.js
@@ -18,29 +18,26 @@ function onChangeAuth(r) {
 	user = r;
 	if(user) {
 		$('.header-wrapper .photo img').attr('src', user.photoURL);
-		$('.create-wrapper img').attr('src', user.photoURL);
 		$('.header-wrapper .info-wrap').css('display', 'flex');
 		$('.header-wrapper .logo i').css('display', 'none');
-		$('.create-wrapper').show();
-		$('.list-wrapper').show();
-		$('.gap-wrapper').css('flex-grow', 0);
-		$('.gap-wrapper .gap-wrap').css('display', 'none');
-		$('.create-wrapper input[name="writer"]').val(user.displayName);
+		$('.room-wrapper').css('display', 'flex');
+		$('.login-wrapper').css('display', 'none');
 		$('.bt-login').css('display', 'none');
 		$('.bt-logout').css('display', 'flex');
+		
+		// $('.create-wrapper img').attr('src', user.photoURL);
+		// $('.create-wrapper input[name="writer"]').val(user.displayName);
 	}
 	else {
 		$('.header-wrapper .photo img').attr('src', '//via.placeholder.com/1x1/333');
-		$('.create-wrapper img').attr('src', '//via.placeholder.com/1x1/fff');
 		$('.header-wrapper .info-wrap').css('display', 'none');
 		$('.header-wrapper .logo i').css('display', 'inline-block');
-		$('.create-wrapper').hide();
-		$('.list-wrapper').hide();
-		$('.gap-wrapper').css('flex-grow', 1);
-		$('.gap-wrapper .gap-wrap').css('display', 'flex');
-		$('.create-wrapper input[name="writer"]').val('');
+		$('.room-wrapper').css('display', 'none');
+		$('.login-wrapper').css('display', 'flex');
 		$('.bt-login').css('display', 'flex');
 		$('.bt-logout').css('display', 'none');
+		// $('.create-wrapper img').attr('src', '//via.placeholder.com/1x1/fff');
+		// $('.create-wrapper input[name="writer"]').val('');
 	}
 }
 
